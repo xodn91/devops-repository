ec67ecb7ea08549ba572a45ff883c83892166a6a
Jason Lee <jason.lee@DevOpss-MacBook-Pro.local>
2021-03-06 15:42:18 -0700

    Comments on deploy.sh

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+
+#telling OS to excute the following commands
+
 cp ~/secrets/terraform.tfvars.asc .
+
+#accessing the terraform.tvars.asc file
+
 gpg --decrypt --nosymkey-cache --output ./terraform.tfvars ./terraform.tfvars.asc
+
+#encrypting terraform.tfvars file
+
 terraform plan
+
+# command to creat an execution plan
+
 terraform apply
+
+#apply changes for the execution plan
+
 sleep 60s
+
+#waits 60seconds for the file to process????
+
 rm ./terraform.tfvars*
+
+#removing unecrypted file from the local directory
