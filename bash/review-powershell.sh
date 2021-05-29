2f4866dba8c122a35a78d89ea9a232050d042ec7
Jason Lee <jason.lee@DevOpss-MacBook-Pro.local>
2021-05-29 14:53:22 -0700

    powershell codes

---
+++ bash/review-powershell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#creating a new user name Jason
+New-ADUser -Name "Jason"
+
+#Copying a file
+Copy-Item -Path C:\File/txt -Destination
+
+#Deleting a file
+Reomve-Item D:\temp\
+
+#Lists the content of a directory
